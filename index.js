ab492b978cea53e83024e672e5da751a1b7d4acc
shudipto <shudipta15-11424@diu.edu.bd>
2021-06-15 17:23:54 +0600

    complete

---
+++ index.js
@@ -0,0 +1,38 @@
+
+    window.onload = function () {
+        document.getElementById('input1').value = 0;
+        document.getElementById('input2').value = 0;
+    }
+
+
+    const select = document.querySelectorAll("select")
+    const input = document.querySelectorAll("input")
+    const API_URL = "http://api.exchangeratesapi.io/v1/latest?access_key=a1db97bbf769b10b047da293ef680761";
+    let html = "";
+    async function currency() {
+        const res = await fetch(API_URL);
+
+        const data = await res.json();
+        const arrKeys = Object.keys(data.rates);
+        const rates = data.rates;
+        arrKeys.map(item => {
+            return html += `<option value=${item}>${item}</option>`;
+        });
+
+        for (let i = 0; i < select.length; i++) {
+            select[i].innerHTML = html;
+        };
+
+        function convert(i, j) {
+            input[i].value = input[j].value * rates[select[i].value] / rates[select[j].value];
+        };
+
+        input[0].addEventListener('keyup', () => convert(1, 0));
+
+        input[1].addEventListener('keyup', () => convert(0, 1));
+
+        select[0].addEventListener('change', () => convert(1, 0));
+        select[1].addEventListener('change', () => convert(0, 1));
+
+    }
+    currency();
