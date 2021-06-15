221c17e33d622041a3118f0d05df37c286d11055
shudipto <shudipta15-11424@diu.edu.bd>
2021-06-15 22:36:10 +0600

    api

--- js/myscript.js
+++ js/myscript.js
@@ -1,38 +1,44 @@
 
 window.onload = function () {
-        document.getElementById('input1').value = 0;
-        document.getElementById('input2').value = 0;
-    }
+    document.getElementById('input1').value = "";
+    document.getElementById('input2').value = "";
+}
 
 
-    const select = document.querySelectorAll("select")
-    const input = document.querySelectorAll("input")
-    const API_URL = "http://data.fixer.io/api/latest?access_key=dcdcae1648e8ce5ca378068ac5417dcb";
-    let html = "";
-    async function currency() {
-        const res = await fetch(API_URL);
-        console.log(res);
-        const data = await res.json();
-        const arrKeys = Object.keys(data.rates);
-        const rates = data.rates;
-        arrKeys.map(item => {
-            return html += `<option value=${item}>${item}</option>`;
-        });
 
-        for (let i = 0; i < select.length; i++) {
-            select[i].innerHTML = html;
-        };
 
-        function convert(i, j) {
-            input[i].value = input[j].value * rates[select[i].value] / rates[select[j].value];
-        };
+const select = document.querySelectorAll("select")
+const input = document.querySelectorAll("input")
+const API_URL = "https://v2.api.forex/rates/latest.json?withCredentials=true?beautify=true&key=f485e4d4-2c33-431e-b615-f007c33ab750";
+let html = "";
 
-        input[0].addEventListener('keyup', () => convert(1, 0));
+async function currency() {
 
-        input[1].addEventListener('keyup', () => convert(0, 1));
+    const res = await fetch('https://api.exchangerate.host/latest');
+    console.log(res);
+    const data = await res.json();
 
-        select[0].addEventListener('change', () => convert(1, 0));
-        select[1].addEventListener('change', () => convert(0, 1));
 
-    }
-    currency();
+    const arrKeys = Object.keys(data.rates);
+    const rates = data.rates;
+    arrKeys.map(item => {
+        return html += `<option value=${item}>${item}</option>`;
+    });
+
+    for (let i = 0; i < select.length; i++) {
+        select[i].innerHTML = html;
+    };
+
+    function convert(i, j) {
+        input[i].value = input[j].value * rates[select[i].value] / rates[select[j].value];
+    };
+
+    input[0].addEventListener('keyup', () => convert(1, 0));
+
+    input[1].addEventListener('keyup', () => convert(0, 1));
+
+    select[0].addEventListener('change', () => convert(1, 0));
+    select[1].addEventListener('change', () => convert(0, 1));
+
+}
+currency();
